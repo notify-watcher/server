1da19e2e6f841053d802d76d69e77734538d34d5
Francisco Sandoval <francisco@mtblc.co>
2019-10-12 05:56:28 -0300

    typo

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const TEMP_DIR_PATH = path.resolve(path.join('.', 'tmp'));
 
 const config = {
   PORT: process.env.PORT || 3000,
-  DATABASE_URL: 'mongodb://localhost/notify-watchers',
+  DATABASE_URL: 'mongodb://localhost/notify-watcher',
   DOWNLOAD_WATCHERS: process.env.DOWNLOAD_WATCHERS || false,
   WATCHERS: [],
   WATCHERS_TEMP_PATH: path.resolve(path.join(TEMP_DIR_PATH, 'watchers')),
