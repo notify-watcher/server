057c861ad8c7b3c8f085de22e0453d450af55f67
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-15 14:52:35 -0300

    download watchers in parallel

--- src/watchers/index.js
+++ src/watchers/index.js
@@ -12,52 +12,59 @@ function ref(branch) {
   return `refs/remotes/origin/${branch}`;
 }
 
-function saveWatchers(names) {
-  console.log('Saving watchers:', names);
+function saveWatchers(names, repoTempPath) {
   names.forEach(name =>
-    fs.copySync(`${tempDirPath}/${name}`, `${downloadedDirPath}/${name}`),
+    fs.copySync(`${repoTempPath}/${name}`, `${downloadedDirPath}/${name}`),
   );
 }
 
 async function setUpWatchers() {
   if (!config.DOWNLOAD_WATCHERS) return;
   config.WATCHERS = [];
+  fs.emptyDirSync(tempDirPath);
   fs.emptyDirSync(downloadedDirPath);
-  for (let i = 0; i < WATCHERS_LIST.length; i += 1) {
-    const watcher = WATCHERS_LIST[i];
-    const { url, branch, commit, watchers } = watcher;
-    fs.emptyDirSync(tempDirPath);
-    console.log('Downloading:', watcher);
-    const repo = await git.Clone(url, tempDirPath);
-    let reference;
-    if (branch) {
-      reference = await repo.getBranch(ref(branch));
-    } else if (commit) {
-      const oid = git.Oid.fromString(commit);
-      reference = await git.Reference.create(repo, ref(branch), oid, 1, '');
-    } else {
-      console.warn(`No branch or commit for remote ${url}`);
-      return;
-    }
-    await repo.checkoutRef(reference);
+  await Promise.all(
+    WATCHERS_LIST.map(async (watcher, index) => {
+      const repoTempPath = `${tempDirPath}/${index}`;
+      const { url, branch, commit, watchers } = watcher;
 
-    const watchersNames = fs
-      .readdirSync(tempDirPath, { withFileTypes: true })
-      .filter(dirent => dirent.isDirectory())
-      .map(({ name }) => name)
-      .filter(name => name[0] !== '.')
-      .filter(name => watchers === ALL_WATCHERS_KEY || watchers.includes(name));
-    saveWatchers(watchersNames);
-    config.WATCHERS = config.WATCHERS.concat(watchersNames);
-  }
+      if (!branch && !commit)
+        return console.warn(`WARN: No branch or commit for i:${index} ${url}`);
+      if (!watchers || watchers.length === 0)
+        return console.warn(`WARN: No watchers for i:${index} ${url}`);
+
+      const repo = await git.Clone(url, repoTempPath);
+      let reference;
+      if (branch) {
+        reference = await repo.getBranch(ref(branch));
+      } else if (commit) {
+        const oid = git.Oid.fromString(commit);
+        reference = await git.Reference.create(repo, ref(branch), oid, 1, '');
+      }
+      await repo.checkoutRef(reference);
+
+      const watchersNames = fs
+        .readdirSync(repoTempPath, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory())
+        .map(({ name }) => name)
+        .filter(name => name[0] !== '.')
+        .filter(
+          name => watchers === ALL_WATCHERS_KEY || watchers.includes(name),
+        );
+      console.log(
+        `i:${index} Saving ${watchersNames} from ${url}@${branch || commit}`,
+      );
+      saveWatchers(watchersNames, repoTempPath);
+      config.WATCHERS = config.WATCHERS.concat(watchersNames);
+    }),
+  );
   fs.emptyDirSync(tempDirPath);
 
   // TODO: Set up crons for running watchers
 }
 
 if (require.main === module) {
-  console.log('a');
-  setUpWatchers().then(() => console.log('Done'));
+  setUpWatchers().then(() => console.log('\nDownloaded watchers'));
 }
 
 module.exports = setUpWatchers;
