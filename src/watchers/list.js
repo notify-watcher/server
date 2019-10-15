76cc502d17a80b67d2211a1977f4b8907b78c174
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-15 14:52:20 -0300

    add more bad examples and todo for atuh

--- src/watchers/list.js
+++ src/watchers/list.js
@@ -1,5 +1,8 @@
 const ALL_WATCHERS_KEY = 'all';
 
+// We can add authentication for a repo using { username, token }
+// Then we clone as https://username:token@github.com/org/repo
+
 const WATCHERS_LIST = [
   {
     url: 'https://github.com/notify-watcher/watchers',
@@ -14,6 +17,14 @@ const WATCHERS_LIST = [
   {
     url: 'https://github.com/notify-watcher/watchers',
     watchers: ALL_WATCHERS_KEY,
+  },
+  {
+    url: 'https://github.com/notify-watcher/watchers',
+    branch: 'master',
+  },
+  {
+    url: 'https://github.com/notify-watcher/watchers',
+    commit: '4e61a833073a56bbe19ff966f09129f6b165f95a',
   },
 ];
 
