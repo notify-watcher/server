27d530fe1c448e3d66831ca18a3f8d3b5ca2bbcf
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-15 17:26:34 -0300

    run auth watchers

--- src/watchers/run-watchers.js
+++ src/watchers/run-watchers.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+const { execute } = require('@notify-watcher/core');
+
 // Keep track of which watchers are running,
 // use name as a key for no auth watchers
 // use name+username as a key for auth watchers
@@ -14,6 +17,7 @@ const Users = [
         notificationTypes: {
           subscribed: ['clientIdd'],
         },
+        snapshot: {},
       },
     },
   },
@@ -27,15 +31,48 @@ function isRunning(id) {
   return MOCK_REDIS[id];
 }
 
+function startRunning(id) {
+  MOCK_REDIS[id] = true;
+}
+
+function stopRunning(id) {
+  MOCK_REDIS[id] = false;
+}
+
 function runWatchersAuth(watchers) {
   watchers.forEach(async watcher => {
-    const { name: watcherName } = watcher;
+    const { name: watcherName, watch } = watcher;
     const users = Users.filter(user => user.subscriptions[watcherName]);
-    users.forEach(user => {
-      const { name: username } = user;
-      const runId = `${watcherName}:${username}`;
-      if (isRunning(runId)) return;
-      // TODO: run
+    users.forEach(async user => {
+      const id = `${watcherName}:${user.name}`;
+      if (isRunning(id)) return;
+
+      startRunning(id);
+      const subscription = user.subscriptions[watcherName];
+      const options = {
+        auth: subscription.auth,
+        snapshot: subscription.snapshot,
+      };
+      const { notifications, error, snapshot } = await execute(watch, options);
+      subscription.snapshot = snapshot;
+      stopRunning(id);
+
+      console.table({
+        watcher: watcherName,
+        previousSnapshot: options.snapshot,
+        newSnapshot: snapshot,
+        notifications: notifications
+          .map(notification => `${notification.key} ${notification.message}`)
+          .join(' - '),
+        error,
+      });
+
+      if (error)
+        console.warn(
+          `ERR: Found error for watcher ${watcherName} for user ${user.name} ${error}`,
+        );
+
+      // TODO: Send notifications
     });
   });
 }
@@ -43,6 +80,7 @@ function runWatchersAuth(watchers) {
 function runWatchersNoAuth(watchers) {
   watchers.forEach(async watcher => {
     const { name } = watcher.name;
+    // eslint-disable-next-line no-useless-return
     if (isRunning(name)) return;
     // TODO: Run
   });
