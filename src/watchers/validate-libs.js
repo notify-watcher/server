ab6f9d051d4f4c30da84c36c69285c0a4d0854fd
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-16 11:39:37 -0300

    comment about validate-libs

--- src/watchers/validate-libs.js
+++ src/watchers/validate-libs.js
@@ -1,4 +1,11 @@
 /* eslint-disable no-console */
+
+// TODO: For now we're asuming that all libs are being passed to the watcher
+// We should change @notify-watcher/core to allow the creation of custom execute
+// functions that pass more libs, and then do the validation against those.
+// This also means that @notify-watcher/core should have a way to obtain the list
+// of libs that are being passed to watchers
+
 const fs = require('fs-extra');
 
 const serverLibs = fs.readdirSync('./node_modules');
