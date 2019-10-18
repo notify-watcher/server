3a4c2f9adf2bfe662edb747c609a64090b9c6e6b
Francisco Sandoval <francisco@mtblc.co>
2019-10-18 13:34:23 -0300

    fix rebase issues

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const Router = require('koa-router');
 const baseRouter = require('./base');
 const usersRouter = require('./users');
+const clientsRouter = require('./clients');
 const watchersRouter = require('./watchers');
 
 const router = new Router();
 router.use('/', baseRouter.routes());
 router.use('/users', usersRouter.routes());
+router.use('/clients', clientsRouter.routes());
 router.use('/watchers', watchersRouter.routes());
 
 module.exports = router;
