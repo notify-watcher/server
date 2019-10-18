b42ca28387607b406310ddd0930173f5971c8114
Francisco Sandoval <francisco@mtblc.co>
2019-10-18 18:22:36 -0300

    unified options for mongoose to avoid warnings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('../lib/mongoose');
 const config = require('./config');
 const setUpWatchers = require('./watchers');
 const app = require('./app');
 
 async function startup() {
-  await mongoose.connect(config.DATABASE_URL);
+  await mongoose.dbConnect(config.DATABASE_URL);
   const { watchers } = await setUpWatchers();
   config.WATCHERS = watchers;
   app.listen(config.PORT);
