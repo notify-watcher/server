a8a176533d059cf5878e74e280043d4abfb3aa31
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-21 14:29:26 -0300

    update list

--- src/watchers/list.js
+++ src/watchers/list.js
@@ -23,7 +23,7 @@ const ALL_WATCHERS_KEY = 'all';
 const WATCHERS_LIST = [
   {
     url: 'https://github.com/notify-watcher/watchers',
-    branch: 'watcher-unired-tag-total',
+    branch: 'master',
     watchers: ['gtd', 'github-notifications', 'vtr', 'unired-tag'],
   },
 ];
