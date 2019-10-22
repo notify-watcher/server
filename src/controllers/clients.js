3e51a18b99abce36519a2d369584039087db1540
Francisco Sandoval <francisco@mtblc.co>
2019-10-21 21:35:50 -0300

    PR comment - use "orFail"

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -3,8 +3,9 @@ const User = require('../models/user');
 
 async function register(ctx) {
   const { email, token, clientData } = ctx.request.body;
-  const user = await User.findOne({ email });
-  if (!user) throw createError.NotFound('No user has this email');
+  const user = await User.findOne({ email }).orFail(() =>
+    createError.NotFound('No user has this email'),
+  );
   const verification = await user.verifyToken(token);
   if (!verification) throw createError.Unauthorized('Invalid code');
   const client = await user.addClient(clientData);
