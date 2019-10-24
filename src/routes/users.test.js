248d209da284d0cb2f1fd36ef8d743abcecf5bfd
Francisco Sandoval <francisco@mtblc.co>
2019-10-23 21:10:36 -0300

    delete user after tests

--- src/routes/users.test.js
+++ src/routes/users.test.js
@@ -1,22 +1,18 @@
-const _ = require('lodash');
 const request = require('../test/supertest');
+const User = require('../models/user');
 const emails = require('../emails');
 
 describe('users route tests', () => {
   describe('POST users/send-token', () => {
     const sendTokenUrl = '/users/send-token';
     const userEmail = 'test1@example.org';
-    let sendTokenEmailSpy;
-
-    beforeEach(async () => {
-      sendTokenEmailSpy = jest.spyOn(emails, 'sendToken');
-    });
+    const sendTokenEmailSpy = jest.spyOn(emails, 'sendToken');
 
     afterEach(async () => {
       sendTokenEmailSpy.mockReset();
     });
 
-    afterAll(async () => User.delete);
+    afterAll(async () => User.deleteOne({ email: userEmail }));
 
     test('to a new user', async () => {
       await request
