e57e388363cad9fee84d7983895cc3b633db9ab3
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 00:09:11 -0300

    email, telegram client stub

---
+++ src/notifications/email.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+const util = require('util');
+const { env } = require('../config');
+
+const LOCAL_ENV = {
+  clientsNotifications: false,
+};
+
+function sendWatcherNotifications(watcherName, clientsNotifications) {
+  if (env.isDev && LOCAL_ENV.clientsNotifications)
+    console.log(
+      `email notifications for ${watcherName} watcher\n`,
+      util.inspect(clientsNotifications, { showHidden: false, depth: 2 }),
+    );
+}
+
+module.exports = sendWatcherNotifications;
