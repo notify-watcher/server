d150688c4bcae899a4094fa7b5db830b44e6be24
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 10:57:47 -0300

    delete DEFAULT_CLIENT_IDS

--- src/notifications/index.test.js
+++ src/notifications/index.test.js
@@ -1,23 +1,12 @@
 const {
-  constants: { CLIENT_KINDS, DEFAULT_CLIENT_IDS },
+  constants: { CLIENT_KINDS },
 } = require('@notify-watcher/core');
-const {
-  CLIENT_KIND_HANDLERS,
-  DEFAULT_CLIENT_FOR_USER_HANDLERS,
-} = require('./');
+const { CLIENT_KIND_HANDLERS } = require('./');
 
 describe('clientKindHandlers', () => {
   it('has a handler for each client kind', () => {
     Object.keys(CLIENT_KINDS).forEach(clientKind =>
       expect(CLIENT_KIND_HANDLERS[clientKind]).toBeDefined(),
-    );
-  });
-});
-
-describe('defaultClientForUserHandlers', () => {
-  it('has a handler for each default client id', () => {
-    Object.keys(DEFAULT_CLIENT_IDS).forEach(clientId =>
-      expect(DEFAULT_CLIENT_FOR_USER_HANDLERS[clientId]).toBeDefined(),
     );
   });
 });
