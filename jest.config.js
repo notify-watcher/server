f7f008196ee08144bd0a52d504d22afcb5baa46d
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 18:43:22 -0300

    refactor: reorder tests and dev lib

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/', 'watchers/'],
-  setupFilesAfterEnv: ['./src/test/jest-setup.js'],
+  setupFilesAfterEnv: ['./src/tests/jest-setup.js'],
 };
