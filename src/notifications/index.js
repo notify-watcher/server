2b50d3d28d46c89cef0444c7f0eec173f1c6696c
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 00:56:38 -0300

    refactor code, extract big logic

--- src/notifications/index.js
+++ src/notifications/index.js
@@ -27,6 +27,7 @@ const MOCK_CLIENTS = {
 const LOCAL_ENV = {
   usersNotifications: false,
   clientsNotifications: false,
+  clientKindsNotifications: false,
 };
 
 const CLIENT_KIND_HANDLERS = {
@@ -47,55 +48,50 @@ function userClientForClientId(user, clientId) {
     : MOCK_CLIENTS[clientId];
 }
 
-/**
- * Receives an array of user-notifications objects and sends the
- * notifications to those users using the configured clients
- * @param {{user: Object, notifications: Object[]}[]} usersNotifications
- */
-function sendWatcherNotifications(watcherName, usersNotifications) {
-  if (env.isDev && LOCAL_ENV.usersNotifications)
-    console.log(
-      'usersNotifications\n',
-      util.inspect(usersNotifications, { showHidden: false, depth: 2 }),
-    );
+function groupUserNotifications({ user, notifications, watcherName }) {
+  // { clientId: { client, notifications } }
+  const userClientNotifications = {};
+  const { notificationTypes } = user.subscriptions[watcherName];
 
-  const clientKindsNotifications = Object.keys(CLIENT_KINDS).reduce(
-    (object, key) => ({ ...object, [key]: [] }),
-    {},
-  );
+  notifications.forEach(notification => {
+    const clientIds = notificationTypes[notification.key];
+    if (!clientIds || clientIds.length === 0) return;
 
-  usersNotifications.forEach(({ user, notifications: userNotifications }) => {
-    // { clientId: { client, notifications } }
-    const userClientNotifications = {};
-    const { notificationTypes } = user.subscriptions[watcherName];
+    clientIds.forEach(clientId => {
+      if (!userClientNotifications[clientId]) {
+        const client = userClientForClientId(user, clientId);
+        if (!client) return;
 
-    userNotifications.forEach(notification => {
-      const clientIds = notificationTypes[notification.key];
-      if (!clientIds || clientIds.length === 0) return;
-
-      clientIds.forEach(clientId => {
-        if (!userClientNotifications[clientId]) {
-          const client = userClientForClientId(user, clientId);
-          if (!client) return;
-
-          userClientNotifications[clientId] = { client, notifications: [] };
-        }
-        userClientNotifications[clientId].notifications.push(notification);
-      });
-    });
-
-    Object.keys(userClientNotifications).forEach(clientId => {
-      const { client, notifications } = userClientNotifications[clientId];
-      clientKindsNotifications[client.kind].push({ client, notifications });
+        userClientNotifications[clientId] = { client, notifications: [] };
+      }
+      userClientNotifications[clientId].notifications.push(notification);
     });
   });
 
-  if (env.isDev && LOCAL_ENV.clientsNotifications)
-    console.log(
-      'clientsNotifications\n',
-      util.inspect(clientKindsNotifications, { showHidden: false, depth: 3 }),
-    );
+  return Object.values(userClientNotifications);
+}
 
+function groupUsersNotifications(usersNotifications, watcherName) {
+  return usersNotifications
+    .map(({ user, notifications }) =>
+      groupUserNotifications({ user, notifications, watcherName }),
+    )
+    .flat();
+}
+
+function groupClientsNotifications(clientsNotifications) {
+  return Object.keys(CLIENT_KINDS).reduce(
+    (clientKindsNotifications, clientKind) => ({
+      ...clientKindsNotifications,
+      [clientKind]: clientsNotifications.filter(
+        ({ client }) => client.kind === clientKind,
+      ),
+    }),
+    {},
+  );
+}
+
+function sendClientKindsNotifications(clientKindsNotifications, watcherName) {
   Object.keys(clientKindsNotifications).forEach(clientKind => {
     const clientHandler = CLIENT_KIND_HANDLERS[clientKind];
     if (!clientHandler) {
@@ -106,6 +102,40 @@ function sendWatcherNotifications(watcherName, usersNotifications) {
     const clientKindNotifications = clientKindsNotifications[clientKind];
     clientHandler(watcherName, clientKindNotifications);
   });
+}
+
+/**
+ * Receives an array of user-notifications objects and sends the
+ * notifications to those users using the configured clients
+ * @param {{user: Object, notifications: Object[]}[]} usersNotifications
+ */
+function sendWatcherNotifications(watcherName, usersNotifications) {
+  const clientsNotifications = groupUsersNotifications(
+    usersNotifications,
+    watcherName,
+  );
+
+  const clientKindsNotifications = groupClientsNotifications(
+    clientsNotifications,
+  );
+
+  sendClientKindsNotifications(clientKindsNotifications, watcherName);
+
+  if (env.isDev && LOCAL_ENV.usersNotifications)
+    console.log(
+      'usersNotifications\n',
+      util.inspect(usersNotifications, { showHidden: false, depth: 2 }),
+    );
+  if (env.isDev && LOCAL_ENV.clientsNotifications)
+    console.log(
+      `clientsNotifications ${watcherName}\n`,
+      util.inspect(clientsNotifications, { showHidden: false, depth: 2 }),
+    );
+  if (env.isDev && LOCAL_ENV.clientKindsNotifications)
+    console.log(
+      `clientKindsNotifications ${watcherName}\n`,
+      util.inspect(clientKindsNotifications, { showHidden: false, depth: 2 }),
+    );
 }
 
 module.exports = {
