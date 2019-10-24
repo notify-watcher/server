0037d9fe36deaa324f36d40f5c04bfba5fd3ba38
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 12:10:21 -0300

    check also that client handler is a function

--- src/notifications/index.test.js
+++ src/notifications/index.test.js
@@ -5,8 +5,10 @@ const { CLIENT_KIND_HANDLERS } = require('./');
 
 describe('clientKindHandlers', () => {
   it('has a handler for each client kind', () => {
-    Object.keys(CLIENT_KINDS).forEach(clientKind =>
-      expect(CLIENT_KIND_HANDLERS[clientKind]).toBeDefined(),
-    );
+    Object.keys(CLIENT_KINDS).forEach(clientKind => {
+      const handler = CLIENT_KIND_HANDLERS[clientKind];
+      expect(handler).toBeDefined();
+      expect(handler).toBeInstanceOf(Function);
+    });
   });
 });
