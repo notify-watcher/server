ef1ca91b3f334ad39964a5a40565ff33ae86aab9
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-24 15:28:17 -0300

    telegram client

--- src/notifications/telegram.js
+++ src/notifications/telegram.js
@@ -1,17 +1,28 @@
 /* eslint-disable no-console */
 const util = require('util');
-const { env } = require('../config');
+const {
+  constants: { CLIENT_KINDS },
+} = require('@notify-watcher/core');
+const { env, clients } = require('../config');
+const axios = require('./axios');
+
+const { url } = clients[CLIENT_KINDS.telegram];
 
 const LOCAL_ENV = {
   clientsNotifications: false,
 };
 
-function sendWatcherNotifications(watcherName, clientsNotifications) {
+async function sendWatcherNotifications(watcherName, clientsNotifications) {
   if (env.isDev && LOCAL_ENV.clientsNotifications)
     console.log(
       `telegram notifications for ${watcherName} watcher\n`,
       util.inspect(clientsNotifications, { showHidden: false, depth: 2 }),
     );
+
+  const chatIdsNotifications = clientsNotifications.map(
+    ({ client: { chatId }, notifications }) => ({ chatId, notifications }),
+  );
+  await axios.post(`${url}/notifications`, chatIdsNotifications);
 }
 
 module.exports = { sendWatcherNotifications };
