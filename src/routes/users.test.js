0b48a7b325ddbdb7034986ad7b327595c519f428
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-28 11:56:48 -0300

    fix routes/users test

--- src/routes/users.test.js
+++ src/routes/users.test.js
@@ -6,17 +6,22 @@ const { HTTP_CODES } = require('../constants');
 describe('users routes', () => {
   describe('POST users/send-token', () => {
     const sendToken = body => request.post('/users/send-token').send(body);
-
-    let response;
-    const email = 'send-token@example.org';
     const sendTokenEmailSpy = jest.spyOn(emails, 'sendToken');
+
+    const email = 'send-token@example.org';
+    let response;
 
     afterAll(() => sendTokenEmailSpy.mockRestore());
 
-    describe('to a new user', () => {
-      beforeEach(async () => {
-        sendTokenEmailSpy.mockClear();
+    describe('to a non-existing user', () => {
+      beforeAll(async () => {
         response = await sendToken({ email });
+      });
+
+      afterAll(async () => {
+        const user = await User.findOne({ email });
+        await user.remove();
+        sendTokenEmailSpy.mockClear();
       });
 
       it('should return "created"', () =>
@@ -31,15 +36,13 @@ describe('users routes', () => {
       beforeAll(async () => {
         user = await User.create({ email });
         await user.createSecret();
-      });
-
-      afterAll(() => user.deleteOne());
-
-      beforeEach(async () => {
         response = await sendToken({ email });
       });
 
-      afterEach(() => sendTokenEmailSpy.mockClear());
+      afterAll(() => {
+        sendTokenEmailSpy.mockClear();
+        user.remove();
+      });
 
       it('should return "no content"', () =>
         expect(response.status).toEqual(HTTP_CODES.noContent));
