b6cc1748b04bc6d62b2c09c606a1dd1e35f22c38
Nicolás Gebauer <negebauerm@gmail.com>
2019-10-30 23:25:53 -0300

    REVERT THIS: run watchers faster

--- src/watchers/index.js
+++ src/watchers/index.js
@@ -18,8 +18,8 @@ async function setUpWatchers() {
   } = loadWatchers(WATCHERS_PATH);
 
   const minuteWatchersCronJob = new CronJob(
-    // '0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', // for dev
-    '0 * * * * *',
+    '0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', // for dev
+    // '0 * * * * *',
     function runMinuteWatchers() {
       console.log(new Date(), 'minute iteration');
       runWatchersAuth(minuteWatchersAuth);
@@ -28,8 +28,8 @@ async function setUpWatchers() {
   );
 
   const hourWatchersCronJob = new CronJob(
-    // '0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', // for dev
-    '0 0 * * * *',
+    '0,5,10,15,20,25,30,35,40,45,50,55 * * * * *', // for dev
+    // '0 0 * * * *',
     function runHourWatchers() {
       console.log(new Date(), 'hour iteration');
       runWatchersAuth(hourWatchersAuth);
