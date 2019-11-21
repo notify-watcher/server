361d91a0f39afeda5e276709ddddd09e8cd2ced5
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-21 18:59:16 -0300

    chore: log request body in dev

--- src/app.js
+++ src/app.js
@@ -3,14 +3,16 @@ const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
 const router = require('./routes');
 const config = require('./config');
-const { errorsMiddleware } = require('./middleware');
+const { errorsMiddleware, logBody } = require('./middleware');
 
 const app = new Koa();
 if (config.env.isDev || config.env.isProd) {
   app.use(logger());
 }
+
 app.use(errorsMiddleware);
 app.use(bodyParser());
+if (config.env.isDev) app.use(logBody);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
