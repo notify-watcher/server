6b1f9bbd00b6bc63d4e86dfa9e87750389e67fd0
Nicolás Gebauer <negebauerm@gmail.com>
2019-11-21 17:48:14 -0300

    only update multiple labels when new version

--- scripts/docker_tags.sh
+++ scripts/docker_tags.sh
@@ -2,7 +2,16 @@
 
 tags() {
   local image_name="notifywatcher1/server"
+  local git_describe=$(git describe)
   local git_tag=$(git describe --abbrev=0)
+
+  if [[ "$git_describe" != "$git_tag" ]]; then
+    # this is not a new version, update only latest
+    echo "${image_name}:latest"
+    return
+  fi
+
+  # new version, update multiple labels
   git_tag=${git_tag/v/}
 
   local mayor_minor_path=(${git_tag//./ })
@@ -10,7 +19,7 @@ tags() {
   local minor="${mayor}.${mayor_minor_path[1]}"
   local patch="${minor}.${mayor_minor_path[2]}"
 
-  echo "${image_name}:latest ${image_name}:${mayor} ${image_name}:${minor} ${image_name}:${patch}"
+  echo "${image_name}:${patch} ${image_name}:${minor} ${image_name}:${mayor} ${image_name}:latest"
 }
 
 echo $(tags)
